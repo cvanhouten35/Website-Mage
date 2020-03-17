63a8300eed933ce64597dda297e4ee14abc81ba3
Antimony Apodaca <jumpsplat120@yahoo.com>
2020-03-17 12:46:14 -0700

    Update classParticle.js

--- js/classParticle.js
+++ js/classParticle.js
@@ -15,6 +15,7 @@ class Particle {
 		this.x     = x     || rand(0, this.canvas.canvas.width)
 		this.y     = y     || rand(0, this.canvas.canvas.height)
 		this.life  = life  || rand(90, 600)
+		this.maxLife = this.life
 		this.color = color || rgbToHex([rand(0, 255), rand(0, 255), rand(0, 255)])
 		this.size  = size  || this.life / 120
 		this.speed = speed || rand(1, 5)
@@ -30,10 +31,10 @@ class Particle {
 	
 	update(dx, dy) {
 		this.x += dx !== undefined ? dx : this.speed * -2
-		this.y += dy !== undefined ? dy : this.speed * -.005
+		this.y += dy !== undefined ? dy : this.speed * -.05
 		this.life  -= rand(1, 3)
 		this.size   = this.life / 120
-		this.speed -= 
+		this.speed  = ((this.maxLife - this.life) / this.maxLife) * this.speedMax
 		this.end    = this.life <= 0
 	}
 }
