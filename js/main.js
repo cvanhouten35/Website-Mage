5424f1a33fefd8ad4dcd1cd45b060443ee695fa4
cvanhouten35 <59463137+cvanhouten35@users.noreply.github.com>
2020-03-25 17:12:51 -0700

    Notes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-/*ON CLOSE IT WILL HAVE A DIALOG BOX THAT WILL POP UP ASKING IF YOUR SURE YOU WANNA CLOSE EBCAUSE IT WILL NOT SAVE AUTOMATICALLY AND WILL HAVE A CLOSE ANYWAY BUTTON, A SAVE NOW BUTTON AND A CANCEL BUTTON
+/*
 	
-Have a title screen with a click any where to continue before having that fade out to have Create a New Character option, Load Exsisting Character in the center of the screen 
-	-have the load Character Sheet read Strings made by the program and load them up into the Character Screen in Play mode
+Have a title screen with a Create a New Character Button, Import Character and any preexisting saves on the device in the center of the screen 
+	-have the import Character Sheet read Strings made by the program and load them up into the Character Screen in Play mode
 
 Creating a new character: first it will have a page with just the Character information (There will be a Fill out later button to skip) then the page will move to the top of the screen to reveal the mechanics sheet below.
 	-There will be a key in the upper left corner with the colors for the Free dots, normal dots and a planned dots option that will be numbered (they will not count against XP and there will be an option to toggle "Planning mode" where you will only fill in Planning dots. Planning dots will create a Wishlist on a optional tab to the side that you can open to rearrange the order of planning dots and see the XP they will cost. 
 The first time you interact with a section (Skills/Attributes/Spheres) It will automactically fill in free dots first until all free dots are spent until it starts using normal XP, and once you are out of normal XP it will switch to planning mode automatically
 
-Load sheet will have an Input Text box underneath that will ask you to paste the string that the text file gave you and will start up in Play mode with last current stats
+Import sheet will have an Input Text box underneath that will ask you to paste the string that the text file gave you and will start up in Play mode with last current stats
 
 Once you are done filling everything out it will have a Save Printable Character Sheet and Save Digital Character Sheet (on hover have note that explains that this will save any inforamtions currently on the play sheet as well as the base character information.) on the bottom
 	-save printable sheet will save a pdf locally that is visually appealing and printable but not editable. The Health/Willpower/Quint will be unfilled.
@@ -15,6 +15,103 @@ Once you are done filling everything out it will have a Save Printable Character
 Playable mode will have Quint,Willpower,Paradox,Health, and paradox flaws editable but nothing else
 
 Once you have saved it will switch to play mode where the dots will no longer be editable and the planning dots will dissapear health, willpower, and Quint will be the only ones that can be changed and an Edit Character Sheet button, Load New Character Sheet will be in the upper right.
+
+PLAYER INFORMATION
+Player Name
+Email Address
+
+MES EXCLUSIVE:
+Membership ID: US2018020046
+Current MC
+MC At Creation
+CCD
+CCD XP
+MC XP
+
+Direct Storyteller
+Storyteller Email
+Direct Coordinator
+Coordinator Email
+
+VSS: 
+Domain
+Region
+Country
+Chronicle
+
+CHARACTER INFORMATION
+Character Name
+Alias
+Concept
+Archetype
+Group (Clan, Tribe, Kith, Affiliation)
+
+Sect
+Apparent Age
+Date of Birth
+First Turn Date
+Native Language:
+Rank (Generation, Seeming, Enlightenment, Rank)
+Title
+
+XP INFORMATION
+Floor XP
+Chronicle XP
+Total Spent XP
+Remaining XP
+
+Initiative
+Power Resource (Blood, Gnosis, Quintessence, Glamour) 
+Willpower
+Health Boxes
+Morality (Humanity, Harano, Banality, Wisdom)
+Sanity (Beast Traits, Seethe, Bedlam)
+
+STATUS/Court Titles/The Mage one/ Renown
+
+ATTRIBUTES
+Physical
+	Focus: Stamina, Dexterity, Strength 
+Social
+	Focus: Manipulation, cHARISMA, Appearance 
+Mental
+	Focus: Intelligence, Wits, Perception 
+
+SKILLS
+Academics: 
+Animal Ken: 
+Athletics: 
+Awareness: 
+Brawl: 
+Computers: 
+Crafts: 
+Dodge: 
+Drive: 
+Empathy: 
+Firearms: 
+Intimidation: 
+Investigation: 
+Leadership: 
+Linguistics: 
+Lore: 
+Medicine: 
+Melee: 
+Occult: 
+Performance: 
+Science: 
+Security: 
+Stealth: 
+Streetwise: 
+Subterfuge: 
+Survival:
+
+BACKGROUNDS
+MERITS
+FLAWS
+Powers (disciplines/Arts/Gifts/Spheres)
+Supplemental Powers (Techniques/Elder Powers, Realms, Rotes)
+Equipment
+EXPERIENCE LOG
 */
 
 function loadCharSheet() {
