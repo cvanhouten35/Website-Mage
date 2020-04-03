deb1de46a117da8034f26d6cc36b6a8e0a2612b6
Antimony Apodaca <jumpsplat120@yahoo.com>
2020-04-03 00:12:43 -0700

    particle update

--- js/classParticle.js
+++ js/classParticle.js
@@ -1,24 +1,24 @@
 class Particle {
 	constructor(canvas) {
 		this.canvas   = canvas
-		this.pos      = new Vector(0, 0)
-		this.size     = 0
+		this.pos      = new Vector(randWhole(0, this.canvas.canvas.width), randWhole(0, this.canvas.canvas.height))
 		this.speed    = 0
 		this.speedMax = 0
-		this.color    = "#FFFFFF"
-		this.life     = 0
+		this.color    = rgbToHex([randWhole(0, 255), randWhole(0, 255), randWhole(0, 255)])
+		this.life     = randWhole(90, 600)
+		this.size     = this.life / 120
 		this.end      = false
 	}
 	
 	initalize(x, y, life, color, size, speed, speedMax) {
-		this.pos.x = x     || rand(0, this.canvas.canvas.width)
-		this.pos.y = y     || rand(0, this.canvas.canvas.height)
-		this.life  = life  || rand(90, 600)
+		this.pos.x = x    || this.pos.x
+		this.pos.y = y    || this.pos.y
+		this.life  = life || this.life
 		this.maxLife = this.life
-		this.color = color || rgbToHex([rand(0, 255), rand(0, 255), rand(0, 255)])
-		this.size  = size  || this.life / 120
-		this.speed = speed || rand(1, 5)
-		this.speedMax = speedMax || rand(30, 50)
+		this.color = color || this.color
+		this.size  = size  || this.size
+		this.speed = speed || randWhole(1, 5)
+		this.speedMax = speedMax || randWhole(30, 50)
 	}
 	
 	draw() {
@@ -31,7 +31,7 @@ class Particle {
 	update(dx, dy) {
 		this.x += dx !== undefined ? dx : this.speed * -2
 		this.y += dy !== undefined ? dy : this.speed * -.05
-		this.life  -= rand(1, 3)
+		this.life  -= randWhole(1, 3)
 		this.size   = this.life / 120
 		this.speed  = ((this.maxLife - this.life) / this.maxLife) * this.speedMax
 		this.end    = this.life <= 0
